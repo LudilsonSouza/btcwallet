b4a5069adf0aa0b97a6d13239a20d347208218e5
Ludilson Souza <ludilson.ac.souza@gmail.com>
2022-11-20 20:04:24 -0300

    Wallet de bitcoin

---
+++ src/createWallet.js
@@ -0,0 +1,8 @@
+//Importar as dependências
+const bip32 = require('bip32')
+const bip39 = require('bip39')
+const bitcoin = require('bitcoinjs-lib')
+
+//Definir a rede
+
+const network = bitcoin.networks.testnet
