6373cf4915485aed47b6c034babea2844b8ce5b7
Ludilson Souza <ludilson.ac.souza@gmail.com>
2022-11-20 20:31:49 -0300

    Derivação da wallet HD

--- src/createWallet.js
+++ src/createWallet.js
@@ -4,5 +4,14 @@ const bip39 = require('bip39')
 const bitcoin = require('bitcoinjs-lib')
 
 //Definir a rede
-
 const network = bitcoin.networks.testnet
+
+//Derivação de wallet HD
+const path = `'m/49'/1'/0'/0`
+
+//Criar o mnemonic para a seed (palavras de senha)
+let mnemonic = bip39.generateMnemonic()
+const seed = bip39.mnemonicToSeedSync(mnemonic)
+
+//Criar a raiz da wallet HD
+let root = bip32.fromSeed(seed, network)
